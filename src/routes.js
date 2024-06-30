e54bbae097f6b5d31f727cc86bc09a298931b7f1
José Cervera <joseluis.justinianocervera1a@gmail.com>
2024-06-30 14:34:07 -0600

    Proyecto inicial

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const { default: Inicio } = require("pages/Inicio");
+const { BrowserRouter, Routes, Route } = require("react-router-dom");
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
