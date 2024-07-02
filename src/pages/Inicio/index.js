b60cacbff2780b7a2dcdd30a198da97cd0b2fa3e
José Cervera <joseluis.justinianocervera1a@gmail.com>
2024-07-01 18:53:27 -0600

    Fetch API

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,9 +2,21 @@ import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
 import styles from "./index.module.css";
-import videos from "data/db.json";
+import { useState, useEffect } from "react";
 
 function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/josecervera20/alura-cinema-api/videos"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data);
+      });
+  }, []);
+
   return (
     <>
       <Banner img="home" color="#154580" />
